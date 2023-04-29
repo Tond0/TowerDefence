211c28173b7cfaf04e5c2245cf8466186b3ff24d
Tondo <tondinialessio@gmail.com>
2023-04-29 17:18:26 +0200

    Nemici + Path + Spawner + Tilemap

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    [Header("Movimento")]
+    [NonSerialized] public float durata_percorso;
+    [NonSerialized] public Transform[] destinazioni;
+    private Movimento movimento_entity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movimento_entity = new Movimento(ConvertiDestinazioni(), transform);
+
+        movimento_entity.ConfigurazionePercorso(durata_percorso);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movimento_entity.finito)
+            Destroy(gameObject);
+
+        movimento_entity.Muovi();
+    }
+
+    //DoPath non accetta il transform quindi devo convertire in vector2
+    private Vector3[] ConvertiDestinazioni()
+    {
+        Vector3[] destinazione_vet = new Vector3[destinazioni.Length];
+
+        for(int i = 0; i < destinazioni.Length; i++)
+        {
+            destinazione_vet[i] = destinazioni[i].position;
+        }
+
+        return destinazione_vet;
+    }
+}
